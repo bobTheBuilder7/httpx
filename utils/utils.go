9dc34f46f541a13ce50dd06d1562d98c04c5f8b7
Suren Adzhamoglyan <adgsuren@gmail.com>
2026-03-10 23:57:37 -0700

    added text response

--- utils/utils.go
+++ utils/utils.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+func TextResponse(w http.ResponseWriter, text string) error {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	_, err := w.Write([]byte(text))
+	return err
+}
+
 func JSONResponse(w http.ResponseWriter, v any) error {
 	w.Header().Set("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(v)
